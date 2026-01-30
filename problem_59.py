fbef69243ba5e639f6982e45360c7e01e2ee0729
vinayprasanth-tech <vinayprasanth0406@gmail.com>
2026-01-30 14:50:36 +0530

    Update problem_59.py

--- problem_59.py
+++ problem_59.py
@@ -4,7 +4,4 @@
 def rotate_list(lst, k):
     n = len(lst)
     k = k % n
-    return lst[k:] + lst[:k]
-
-numbers = [1, 2, 3, 4, 5]
-print(f"Rotated by 2: {rotate_list(numbers, 2)}")
+    return lst[-k:] + lst[:-k]
