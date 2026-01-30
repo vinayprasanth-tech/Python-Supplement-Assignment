2f476df061088fdd7f712b68003133eb56237503
vinayprasanth-tech <vinayprasanth0406@gmail.com>
2026-01-30 14:36:40 +0530

    Update problem_52.py

--- problem_52.py
+++ problem_52.py
@@ -3,9 +3,7 @@
 
 def find_missing(numbers):
     n = len(numbers) + 1
-    expected_sum = n * (n + 1) / 2
+    expected_sum = n * (n + 1) // 2   # FIXED
     actual_sum = sum(numbers)
     return expected_sum - actual_sum
 
-nums = [1, 2, 3, 4, 6, 7, 8, 9, 10]
-print(f"Missing number: {find_missing(nums)}")
