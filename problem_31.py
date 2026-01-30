9421d24ca8eadc73a464ab1d2873bfaa5b78d5bf
vinayprasanth-tech <vinayprasanth0406@gmail.com>
2026-01-30 13:38:30 +0530

    Update problem_31.py

--- problem_31.py
+++ problem_31.py
@@ -1,7 +1,4 @@
-# Problem 31: Concatenate strings
-# Find and fix the error
-
 first_name = "John"
 age = 25
-message = "My name is " + first_name + " and I am " + age + " years old"
+message = "My name is " + first_name + " and I am " + str(age) + " years old"
 print(message)
