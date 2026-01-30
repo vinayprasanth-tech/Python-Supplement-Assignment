02d292180b5eeb0955c4265a6a5fdc0bc8cb54a2
vinayprasanth-tech <vinayprasanth0406@gmail.com>
2026-01-30 13:57:11 +0530

    Update problem_37.py

--- problem_37.py
+++ problem_37.py
@@ -2,9 +2,12 @@
 # Find and fix the error
 
 def is_leap_year(year):
-    if year % 4 == 0:
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
     else:
         return False
 
 print(f"Is 1900 a leap year? {is_leap_year(1900)}")
+print(f"Is 2000 a leap year? {is_leap_year(2000)}")
+print(f"Is 2024 a leap year? {is_leap_year(2024)}")
+
