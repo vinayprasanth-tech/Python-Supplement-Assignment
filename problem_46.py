7f1b6536b8c41760ad924874753ea45c97909d3c
vinayprasanth-tech <vinayprasanth0406@gmail.com>
2026-01-30 14:29:02 +0530

    Update problem_46.py

--- problem_46.py
+++ problem_46.py
@@ -1,8 +1,8 @@
 # Problem 46: Find product of all numbers in a list
 # Find and fix the error
-
 numbers = [2, 3, 4, 5]
-product = 0
+product = 1   # FIXED
 for num in numbers:
     product *= num
 print(f"Product: {product}")
+
