947a0a18fb7de34c9a1c8b6b28123e3294a48383
vinayprasanth-tech <vinayprasanth0406@gmail.com>
2026-01-30 13:40:08 +0530

    Update problem_32.py

--- problem_32.py
+++ problem_32.py
@@ -1,9 +1,8 @@
-# Problem 32: Find minimum in a list
-# Find and fix the error
-
 numbers = [45, 12, 78, 34, 89]
 minimum = numbers[0]
-for num in numbers:
+
+for num in numbers[1:]:
     if num < minimum:
         minimum = num
+
 print(f"Minimum: {minimum}")
