75327886aaab6d6544f3b2cc31af5c933bdb2d68
vinayprasanth-tech <vinayprasanth0406@gmail.com>
2026-01-30 14:25:21 +0530

    Update problem_43.py

--- problem_43.py
+++ problem_43.py
@@ -2,7 +2,7 @@
 # Find and fix the error
 
 principal = 1000
-rate = 5
+rate = 5.0   # percentage
 time = 2
 simple_interest = (principal * rate * time) / 100
 print(f"Simple Interest: {simple_interest}")
