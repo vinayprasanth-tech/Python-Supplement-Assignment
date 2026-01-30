0121113dcf35c87b4b1a68289b757cf1c15dee2c
vinayprasanth-tech <vinayprasanth0406@gmail.com>
2026-01-30 14:40:25 +0530

    Update problem_53.py

--- problem_53.py
+++ problem_53.py
@@ -2,8 +2,6 @@
 # Find and fix the error
 
 def are_anagrams(str1, str2):
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
     return sorted(str1) == sorted(str2)
-
-word1 = "Listen"
-word2 = "Silent"
-print(f"Are anagrams: {are_anagrams(word1, word2)}")
