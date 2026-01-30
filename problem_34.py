02dcfcfe6459d9b0e9eece7844737200fba45642
vinayprasanth-tech <vinayprasanth0406@gmail.com>
2026-01-30 13:49:42 +0530

    Update problem_34.py

--- problem_34.py
+++ problem_34.py
@@ -1,8 +1,6 @@
-# Problem 34: Copy a list
-# Find and fix the error
-
 original = [1, 2, 3, 4, 5]
-copy = original
+copy = original.copy()
 copy.append(6)
+
 print(f"Original: {original}")
 print(f"Copy: {copy}")
